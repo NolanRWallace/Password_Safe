26f24b79fedf2af31a51a85753b7c06d32d35d4d
Nolan Wallace <NolanrWallace03@gmail.com>
2020-09-06 16:38:06 -0700

    revert last change

--- client/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user_views import *
-from .password_views import *
-from .email_views import *
-from .combo_views import *
--- client/views/email_views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-from django.contrib import messages
-from ..models import User, Combo, Emails, Passwords
-import bcrypt
